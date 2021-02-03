e3b5c1ca5e9ac7b8154f5d1cc9c62e147dcece54
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2021-02-04 00:16:55 +1100

    changed api endpoint and temp units

--- website/app.js
+++ website/app.js
@@ -1,11 +1,12 @@
-/* Global Variables */
-const owmAppId = 'default-application_4992067';
-const owmAppKey = '613a506d57msh110e01a339ef205p12e3e1jsn07202a303361';
-const owmBaseURL = 'community-open-weather-map.p.rapidapi.com'
+// cors workaround due to no localhost support when running in web
+const corsFix = "https://cors-anywhere.herokuapp.com/"
 
+/* Global Variables */
+const owmAppKey = '2e3f5bd7564a71e84934e70a7dc2eed1';
+const owmBaseURL = 'api.openweathermap.org/data/2.5';
 //Get request to fetch weather data
 const getTemp = async (url, zipcode, appKey) => {
-    const response = await fetch(`https://${url}/weather?zip=${zipcode}`, {
+    const response = await fetch(`${corsFix}http://${url}/weather?zip=${zipcode},AU&APPID=${appKey}&units=metric`, {
         method: 'GET',
         headers: {
             'X-RapidAPI-Key': appKey,
@@ -32,7 +33,7 @@ function callBack(e) {
     const newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
     // Use weatherData from getTemp in chained post request to server.js
-    getTemp(owmBaseURL, `${newZip},au`, owmAppKey).then((weatherData) => {
+    getTemp(owmBaseURL, newZip, owmAppKey).then((weatherData) => {
         const body = {
             temp: weatherData.main.temp,
             date: newDate,
@@ -41,9 +42,6 @@ function callBack(e) {
         postData('/submit', body)
     })
 };
-
-// Convert kelvin value to degrees celsius
-const toCelsius = (temp) => `${(temp - 273.15).toFixed(1)}°C`;
 
 // Post request to server.js
 const postData = async (url, data = {}) => {
@@ -81,6 +79,6 @@ const getData = async (url = '/all') => {
 // Dynamically update UI with projectData values
 const displayData = (date = '', temp = 0, content = '') => {
     document.getElementById('date').innerHTML = date;
-    document.getElementById('temp').innerHTML = toCelsius(temp);
+    document.getElementById('temp').innerHTML = `${temp}°C`;
     document.getElementById('content').innerHTML = content;
 };
